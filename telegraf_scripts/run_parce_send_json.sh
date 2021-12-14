0754fd7d8fe076a847ce1cf3bb53b6d745c9e557
Mykhailo <mikhail.dubik@gmail.com>
2021-12-14 20:06:19 +0200

    add telegram send parce send json

---
+++ telegraf_scripts/run_parce_send_json.sh
@@ -0,0 +1,18 @@
+[[inputs.exec]]
+  ## Commands array
+commands = [
+  "/etc/telegraf/get_node_state.sh"
+ ]
+
+  ## Timeout for each command to complete.
+ timeout = "5s"
+
+  ## measurement name suffix (for separating different commands)  name_suffix = "_node_status"
+
+  ## Data format to consume.
+  ## Each data format has its own unique set of configuration options, read
+  ## more about them here:
+  ## https://github.com/influxdata/telegraf/blob/master/docs/DATA_FORMATS_INPUT.md
+  data_format = "value"
+  data_type = "string" # required
+
